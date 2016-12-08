6f26f4a7a627f92954a2c9dcddd5318d146d8833
Kevin <aoxianglele2010@gmail.com>
2016-12-08 11:06:24 +0800

    Initial Commit

---
+++ Printer/Printer.h
@@ -0,0 +1,19 @@
+//
+//  Printer.h
+//  Printer
+//
+//  Created by GongXiang on 12/8/16.
+//  Copyright © 2016 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Printer.
+FOUNDATION_EXPORT double PrinterVersionNumber;
+
+//! Project version string for Printer.
+FOUNDATION_EXPORT const unsigned char PrinterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Printer/PublicHeader.h>
+
+
